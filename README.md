5f20b0570967336f33f65f6c9781b72c43e0f361
Nonetrix <nonetrix@protonmail.com>
2021-01-31 12:14:18 -0600

    a

---
+++ README.md
@@ -0,0 +1,14 @@
+# TermPomodoro
+Simple terminal pomodoro implementation in C++
+
+compiling and running
+
+    g++ ./src/main.cpp -Ofast -lsfml-audio
+    ./a.out
+
+dependencies
+
+arch
+
+    sudo pacman -S sfml
+    
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <unistd.h>
+#include <SFML/Audio.hpp>
+
+//This sleeps for time_wait then plays sound
+void play_sound_after(int time_wait, sf::Music& sound)
+{
+	std::cout << "Started timer" << std::endl;
+	sleep(time_wait);
+	sound.play();
+    std::cin.clear();
+    std::cin.ignore ( 1024, '\n' );
+    std::cout << "Time up!\n";
+    sleep(3);
+	sound.stop();
+}
+
+int main() {
+    //This is the intager that stores the users input
+    int input;
+
+    //This is the audio file that plays when the timer is up
+    sf::Music sound;
+    if (!sound.openFromFile("./files/sound.wav")) 
+    {
+        std::cout << "Could not load audio file";
+        return -1;
+    }
+
+    while(1) 
+    {
+        //Clears screen
+        std::cout << "\x1B[2J\x1B[H";
+
+        //Asks the user what they would like to do
+		std::cout << "How long would you want to take a break for or work for?" << std::endl << std::endl;
+
+		std::cout << "1. Work for 25 mins"         << std::endl;
+		std::cout << "2. Take a break for 5 mins"  << std::endl;
+		std::cout << "3. Take a break for 10 mins" << std::endl << std::endl;
+
+        std::cout << ":";
+        std::cin >> input;
+        
+        //Checks the Input starting with 3 since we all know you are lazy
+        if(input == 3) 
+        {
+            std::cout << "Taking a break for 10 mins" << std::endl;
+            play_sound_after(600, sound);
+
+        } 
+        
+        else if (input == 2) 
+        {
+            std::cout << "Taking a break for 5 mins" << std::endl;
+            //play_sound_after(1, sound); <-- This is for testing
+            play_sound_after(300, sound);
+        } 
+        
+        else if (input == 1) 
+        {
+            std::cout << "Working for 25 mins" << std::endl;
+            play_sound_after(1500, sound);
+        }
+        
+        else 
+        {
+            std::cin.clear();
+            std::cin.ignore ( 1024, '\n' );
+            std::cout << "Invaild input!" << std::endl;
+            sleep(1);
+        }
+    }
+    return 0;
+}
