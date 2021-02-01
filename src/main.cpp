32f56028d0687a16707ae66ff3c2072e51dec2f2
nonetrix <45698918+nonetrix@users.noreply.github.com>
2021-01-31 18:00:41 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,74 @@
 #include <unistd.h>
 #include <SFML/Audio.hpp>
 
+void play_sound_after(int time_wait, sf::Music& sound)
+{
+	std::cout << "Started timer" << std::endl;
+	sleep(time_wait);
+	sound.play();
+    std::cin.clear();
+    std::cin.ignore ( 1024, '\n' );
+    std::cout << "Time up!\n";
+    sleep(3);
+	sound.stop();
+}
+
 int main() {
     //This is the intager that stores the users input
     int input;
 
     //This is the audio file that plays when the timer is up
-    sf::Music music;
-    if (!music.openFromFile("./files/sound.wav"))
-        //TODO this error triggers due to a permission error but it works anyway :/?  
-        std::cout << "This error sometimes triggers due to a harmless permission error?!.. If it triggers for another reason well your fucked I guess :D";
+    sf::Music sound;
+    if (!sound.openFromFile("./files/sound.wav")) 
+    {
+        std::cout << "Could not load audio file";
+        return -1;
+    }
 
-    while(1) {
+    while(1) 
+    {
         //Clears screen
         std::cout << "\x1B[2J\x1B[H";
 
         //Asks the user what they would like to do
-        std::cout << "\nHow long would you want to take a break for or work for?\n\n1. Work for 25 mins\n2. Take a break for 5 mins\n3. Take a break for 10 mins\n\n:";
+		std::cout << "How long would you want to take a break for or work for?" << std::endl << std::endl;
+
+		std::cout << "1. Work for 25 mins"         << std::endl;
+		std::cout << "2. Take a break for 5 mins"  << std::endl;
+		std::cout << "3. Take a break for 10 mins" << std::endl << std::endl;
+
+        std::cout << ":";
         std::cin >> input;
         
         //Checks the Input starting with 3 since we all know you are lazy
-        if(input == 3) {
+        if(input == 3) 
+        {
             std::cout << "\nTaking a break for 10 mins\n";
-            sleep(600);
-            std::cout << "Time is up!\n";
-            music.play();
-            sleep(10);
-            music.stop();
+            play_sound_after(600, sound);
 
-        } else if (input == 2) {
-            std::cout << "\nTaking a break for 5 mins\n";
-            sleep(300);
-            std::cout << "Time is up!\n";
-            music.play();
-            sleep(10);
-            music.stop();
+        } 
         
-        //Note invaild input will trigger this
-        } else {
+        else if (input == 2) 
+        {
+            std::cout << "\nTaking a break for 5 mins\n";
+            //play_sound_after(1, sound); <-- this is for testing
+            play_sound_after(300, sound);
+            
+        } 
+        
+        else if (input == 1) 
+        {
             std::cout << "\nWorking for 25 mins\n";
-            sleep(1500);
-            std::cout << "Time is up!\n";
-            music.play();
-            sleep(10);
-            music.stop();
+            play_sound_after(1500, sound);
+        }
+        
+        else 
+        {
+            std::cin.clear();
+            std::cin.ignore ( 1024, '\n' );
+            std::cout << "Invaild input!\n";
+            sleep(1);
         }
     }
+    return 0;
 }
